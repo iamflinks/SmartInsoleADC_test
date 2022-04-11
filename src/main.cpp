10e30b74b1ac3a8f8c22fa57e5a791d2833cd013
unknown <olugbonfemi@gmail.com>
2022-04-11 13:40:06 -0400

    Added adc read functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
 
+#define adcPIN 34
+uint16_t binary = 0;
+unsigned long loopInterval = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Resistance measurement");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  binary = analogRead(adcPIN);
+  if (millis() - loopInterval >= 1000)
+  {
+    // Convert to resistance
+    float resistance = (4700 * (binary * 3.3 / 4096)) / (3.3 - (binary * 3.3 / 4096));
+    Serial.print("Resistance:  ");Serial.print(resistance);Serial.print('\n');
+  }
+  
 }

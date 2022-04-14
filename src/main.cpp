6e11589636c115b9960c6f83170b1e9836cfea58
FemiOlugbon <olugbon@gmail.com>
2022-04-14 14:49:25 -0400

    Added Voltage, resistance, and raw binary display

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,26 @@
 #include <Arduino.h>
 
-#define adcPIN 34
+const int adcPIN = 34;
 uint16_t binary = 0;
 unsigned long loopInterval = 0;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Resistance measurement");
+  delay(500);
 }
 
 void loop() {
   binary = analogRead(adcPIN);
   if (millis() - loopInterval >= 1000)
   {
+    loopInterval += 1000;
     // Convert to resistance
+    float voltage = binary * 3.3 / 4096;
     float resistance = (4700 * (binary * 3.3 / 4096)) / (3.3 - (binary * 3.3 / 4096));
-    Serial.print("Resistance:  ");Serial.print(resistance);Serial.print('\n');
+    Serial.print("Voltage:  ");Serial.print(voltage);Serial.print('\n');
+    Serial.print("Raw:  ");Serial.print(binary);Serial.print('\n');
+    Serial.print("Resistance :  ");Serial.print(voltage);Serial.print('\n');
   }
   
 }
